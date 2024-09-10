d0600b68c373fc28637f0a1ef3b634abd36d3cb1
matthew-mckinley1 <matthew.mckinley@ucas-edu.net>
2024-09-10 10:43:03 -0600

    first commit

---
+++ whoAreYou/main.py
@@ -0,0 +1,6 @@
+#Matthew McKinley Who are you?
+
+name = input("What is your name?")
+age = input("How old are you?")
+favcolor = input("What is your favorite color?")
+print("Your name is",name,", You are",age,"years old,", "And your favorite color is",favcolor)
