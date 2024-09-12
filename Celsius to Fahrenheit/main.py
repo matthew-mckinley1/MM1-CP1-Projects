dd994da7218ef39d68d4198014ccd8320be010d8
matthew-mckinley1 <matthew.mckinley@ucas-edu.net>
2024-09-12 10:51:19 -0600

    did most of the project but still need to debug idk why its not working with types and ints

---
+++ Celsius to Fahrenheit/main.py
@@ -0,0 +1,7 @@
+#Matthew McKinley Celsius to Fahrenheit
+
+celsius = input("What celsius temperature do you want changed?")
+celsius = int
+fahrenheit = int((celsius*9 /5)+32)
+str(fahrenheit)
+print(celsius, fahrenheit)
