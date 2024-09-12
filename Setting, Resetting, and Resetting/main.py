95ee0eb5bc2c9013a683c0740461ec5c9b3e9328
matthew-mckinley1 <matthew.mckinley@ucas-edu.net>
2024-09-12 10:30:43 -0600

    full setting project

---
+++ Setting, Resetting, and Resetting/main.py
@@ -0,0 +1,15 @@
+#Matthew McKinley Setting, Resetting, and Resetting
+#first setting
+facultystaff = 32
+students = 100
+guests = students*2
+total = guests+students+facultystaff
+table = total/12
+#then resetting
+newstudents = 99
+newfacultystaff = 29
+newguests = 185
+newspecial = 1
+newtotal = newstudents+newfacultystaff+newguests+newspecial
+print(newtotal/12)
+#should be 312 people or 26 tables
