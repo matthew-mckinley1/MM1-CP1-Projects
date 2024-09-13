2f80b524270c7f19f6c663673f24b4c4cfb00250
matthew-mckinley1 <matthew.mckinley@ucas-edu.net>
2024-09-13 10:27:43 -0600

    Fixes to project still testing

--- Celsius to Fahrenheit/main.py
+++ Celsius to Fahrenheit/main.py
@@ -1,7 +1,8 @@
 #Matthew McKinley Celsius to Fahrenheit
 
 celsius = input("What celsius temperature do you want changed?")
-celsius = int
-fahrenheit = int((celsius*9 /5)+32)
-str(fahrenheit)
-print(celsius, fahrenheit)
+celsius = int(celsius)
+fahrenheit = float((celsius * 9/5)+32)
+fahrenheit = str(fahrenheit)
+celsius = str(celsius)
+print("When it is "+celsius,"degrees celsius, it is "+fahrenheit,"degrees fahrenheit")
