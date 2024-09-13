b8c7ba6939fd9aee8b4ebd1c7e091091f53d67c5
matthew-mckinley1 <matthew.mckinley@ucas-edu.net>
2024-09-13 10:51:29 -0600

    Most of MadLib project, continue when you finish stuff

---
+++ MadLib/main.py
@@ -0,0 +1,13 @@
+#Matthew McKinley MadLib
+
+#name and color use 2 times
+name = input("What is a name?")
+color = input("What is a color?")
+color2 = input("What is a second color?")
+noun = input("What is a noun?")
+adjective = input("What is an adjective?")
+vehicle = input("What is a vehicle?")
+place = input("What is a place?")
+place2 = input("What is another place?")
+
+print("Once upon a time, there was a young man called "+name, "They liked to drive in their "+color, vehicle, "while "+noun, ". When they were at "+place2,"He saw a " +color2, noun "He decided to go to " )
