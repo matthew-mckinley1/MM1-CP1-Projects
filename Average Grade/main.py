5488e0960ce29f84ba1b2a3f62cb2c0a0bdd50a9
matthew-mckinley1 <matthew.mckinley@ucas-edu.net>
2024-09-17 10:34:03 -0600

    literally the only thing and combination i dont try why why why

--- Average Grade/main.py
+++ Average Grade/main.py
@@ -5,10 +5,18 @@ grade1 = input("What is the grade in your first class?")
 grade2 = input("What is the grade in your second class?")
 grade3 = input("What is the grade in your third class?")
 gradeadv = input("What is the grade in your advisory class?")
-lunch = "lunch is cool i like lunch i wish lunch was longer"
 grade6 = input("What is the grade in your sixth class?")
 grade7 = input("What is the grade in your seventh class?")
 grade8 = input("What is the grade in your eigth class?")
 
+grade1 = float(grade1)
+grade2 = float(grade2)
+grade3 = float(grade3)
+gradeadv = float(gradeadv)
+grade6 = float(grade6)
+grade7 = float(grade7)
+grade8 = float(grade8)
 
-print("Your average grade or GPA is"+grade1 +grade2 +grade3 +gradeadv +lunch +grade6 +grade7 +grade8)
+totalaverage = (grade1 + grade2 + grade3 + gradeadv + grade6 + grade7 + grade8) / 7
+float(totalaverage)
+print("Your average grade or GPA is", totalaverage)
