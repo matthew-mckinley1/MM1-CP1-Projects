22dd89c2f37c826d66dff67aea2eeeaedf78a339
matthew-mckinley1 <matthew.mckinley@ucas-edu.net>
2024-09-19 10:47:43 -0600

    AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA

--- Fibbonacci Sequence Generator/main.py
+++ Fibbonacci Sequence Generator/main.py
@@ -1,14 +1,2 @@
-#Matthew McKinley Fibbonacci Sequence Generator
+#fibbonacci sequencec genearator i hate this project Matthew McKinley
 
-num3 = 0
-def function():
-    while num3 <= 3000:
-        num1 = 0
-        int(num1) == num1
-        num2 = 1
-        int(num2) == num2
-        num3 = num1+num2
-        printo = 0
-        print(printo)
-        printo+=num3
-function()
