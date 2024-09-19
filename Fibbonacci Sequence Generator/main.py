b33057cba4bd8a516993703d7d7164410944bfad
matthew-mckinley1 <matthew.mckinley@ucas-edu.net>
2024-09-19 10:20:11 -0600

    bruh i dont even know i reset everything and it still doesnt work ifoepjaifepsajfesa

--- Fibbonacci Sequence Generator/main.py
+++ Fibbonacci Sequence Generator/main.py
@@ -1,17 +1,17 @@
 #Matthew McKinley Fibbonacci Sequence Generator
 
-num1 = 0
-num2 = 1
-num3 = num1+num2
-printo = ""
 
-for num3 in (0,3000):
-    num1 = num2
-    num2 = num3
+def function():
+    num1 = 0
+    num2 = 1
     num3 = num1+num2
-    printo+=str(num1)
-    num1 = int(num1)
+    printo = ""
     print(printo)
+    printo+=num3
+
+
+#while num3 in (0,3000):
+
 
 
 #print(printo)
