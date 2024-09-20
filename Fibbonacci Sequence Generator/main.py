65e7b4f48f385aec54a17f52deb9b264633fc6d8
matthew-mckinley1 <matthew.mckinley@ucas-edu.net>
2024-09-20 10:21:35 -0600

    I HATE THIS GAME OIJWPA JHOHEIESHY:OIWAAJ R:OIJ #RNE jbjnlkmdsngralvdzpok[wak

--- Fibbonacci Sequence Generator/main.py
+++ Fibbonacci Sequence Generator/main.py
@@ -1,2 +1,16 @@
 #fibbonacci sequencec genearator i hate this project Matthew McKinley
 
+
+
+totalruns = 20
+num1 = 0
+num2 = 1
+num3 = num2  
+runs = 1
+
+while totalruns >= runs:
+    print(num3, end=" ")
+    runs += 1
+    num1, num2 = num2, num3
+    num3 = num1 + num2
+print()
