92847a12acdb1b07dd6ee3268a6d4af6cac2ccbb
matthew-mckinley1 <matthew.mckinley@ucas-edu.net>
2024-09-23 10:23:16 -0600

    ez ez ez ez ez ez ez ez ez ez

--- MadLib/main.py
+++ MadLib/main.py
@@ -1,15 +1,14 @@
 #Matthew McKinley MadLib
-#IN PROGRESS!!!!!
-#IN PROGRESS!!!!!
 
 #name and color use 2 times
-name = input("What is a name?")
+name = input("What is a boys name?")
 color = input("What is a color?")
 color2 = input("What is a second color?")
+verb = input("What is a verb ending in 'ing'?")
 noun = input("What is a noun?")
 adjective = input("What is an adjective?")
 vehicle = input("What is a vehicle?")
 place = input("What is a place?")
 place2 = input("What is another place?")
 
-print("Once upon a time, there was a young man called "+name, "They liked to drive in their "+color, vehicle, "while "+noun, ". When they were at "+place2,"He saw a " +color2, noun "He decided to go to " )
+print("Once upon a time, there was a young man called ", name, "He liked to drive in their ", color, vehicle, "while ", verb, "They were on their way to ",place, ". When they were at ", place2,"He saw a " , color2, noun, "He decided to go to inspect the", adjective, noun, "and saw he was terrified of it and went back home!"  )
