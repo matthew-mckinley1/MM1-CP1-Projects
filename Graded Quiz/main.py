8e9b5aa675429c80ec8f133812f44ef212bb5b79
matthew-mckinley1 <matthew.mckinley@ucas-edu.net>
2024-09-24 10:50:54 -0600

    waffles are better than pancakes

--- Graded Quiz/main.py
+++ Graded Quiz/main.py
@@ -1,39 +1,39 @@
 #Matthew McKinley Graded Quiz
 
-print("It will ask questions, make sure to answer yes or no and have them uncapitalized")
+print("It will ask questions, make sure to answer TRUE or FALSE and have them uncapitalized")
 
 score = 0
 
-q1 = input("Are blobfish ugly?")
-if q1 == "no":
+q1 = input("Does the adult human skeleton have 206 bones?")
+if q1 == "true":
     score+=1
 else:
     print("Sorry, you got this one wrong!")
 
 
-q2 = input("Are goldfish better than dogs?")
-if q2 == "yes":
+q2 = input("Is the amazon river the longest river in the world?")
+if q2 == "false":
     score+=1
 else:
     print("Sorry, you got this one wrong!")
 
 
-q3 = input("Are pancakes better than waffles?")
-if q3 == "no":
+q3 = input('Is a group of crows called a "murder"')
+if q3 == "true":
     score+=1
 else:
     print("Sorry, you got this one wrong!")
 
 
-q4 = input("Are dinosaur-shaped chicken nuggets a calzone?")
-if q4 == "yes":
+q4 = input("Can I jump higher than the Eiffel Tower")
+if q4 == "true":
     score += 1
 else:
     print("Sorry, you got this one wrong!")
 
 
-q5 = input("Is a tomato a fruit?")
-if q5 == "no":
+q5 = input("Do dolphins sleep with both eyes closed?")
+if q5 == "false":
     score += 1
 else:
     print("Sorry, you got this one wrong!")
