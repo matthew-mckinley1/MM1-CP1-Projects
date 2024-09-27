238a67c4fb7a45ab9fef774129a099865e7933f5
matthew-mckinley1 <matthew.mckinley@ucas-edu.net>
2024-09-27 10:41:09 -0600

    iggypay piggy yay

---
+++ Piggy Latinny convertino/main.py
@@ -0,0 +1,8 @@
+#Matthew McKinley Pig Latin convertor
+
+
+def piggy(word):
+    firstlet=word[0]+"ay"
+    word = word[1:]
+    return(word+firstlet).title()
+print(piggy(word = input("What would you like to convert to pig latin")))
