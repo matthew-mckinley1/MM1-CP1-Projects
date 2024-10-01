348f89c8c97743fbaef6a590cc7526fed95eefae
matthew-mckinley1 <matthew.mckinley@ucas-edu.net>
2024-10-01 10:50:25 -0600

    uh idk idk i dont quite know

--- Secret Cypher/main.py
+++ Secret Cypher/main.py
@@ -1,2 +1,8 @@
 #Matthew McKinley Secret Cyper
 
+secret = input("What would you like to be converted?")
+shift = input("How much would you like it to be shifted?")
+secret = list(secret)
+
+secret = ord(shift)
+print(secret)
