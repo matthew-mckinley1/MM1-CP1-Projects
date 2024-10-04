5d7644d659c23a628e5978e6137d48f4ee579593
matthew-mckinley1 <matthew.mckinley@ucas-edu.net>
2024-10-04 10:23:41 -0600

    torch note maw creature something wrong oops silly little frindldoodle

---
+++ List of Classes/main.py
@@ -0,0 +1,12 @@
+#Matthew McKinley List of Classes
+
+classes = ["Geography","Computer Science","English","Advisory","Lunch","Math","Drawing","Chemistry"]
+print(classes)
+print(classes[0])
+print(classes[1])
+print(classes[2])
+print(classes[3])
+print(classes[4])
+print(classes[5])
+print(classes[6])
+print(classes[7])
