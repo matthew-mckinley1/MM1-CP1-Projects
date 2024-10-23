bd266ae2393ab6de8d5afa1296d670b34a7e2dea
matthew-mckinley1 <matthew.mckinley@ucas-edu.net>
2024-10-23 10:42:10 -0600

    ok

--- What are these numbers/main.py
+++ What are these numbers/main.py
@@ -1,10 +1,15 @@
 #Matthew McKinley What ARE these numbers
 
-num = input("What number would you like to change?")
-num = int(num)
-comma = "The number with thousand seperators is {num}"
-float = "The number with 4 decimal places is"
-percent = "The number as a percentage is "
-dollar = "The number as a dollar amount is {num:.2f} dollars"
+x = float(input("What number would you like to change?"))
 
-print("The number as a dollar amount is {num:.2f} dollars".format(num))
+thous = "The number in thousands is {tho:,.0f}"
+print(thous.format(tho = x))
+
+dollar = "The number as a dollar amount is {price:.2f} dollars"
+print(dollar.format(price = x))
+
+floaty = "The number as a float with 4 decimal places is {floaty:.4f}"
+print(floaty.format(floaty = x))
+
+percent = "The number as a percentage is {percents:.0%}"
+print(percent.format(percents = x))
