8733e6466d57d18c6d33d03c2ef1d7b1db87d609
matthew-mckinley1 <matthew.mckinley@ucas-edu.net>
2024-11-01 10:51:12 -0600

    bleahioafeoapjfi emanpicoeaj fieoamcajfioepamc get the main thing going ig

---
+++ Rock, Paper, Scissors/main.py
@@ -0,0 +1,38 @@
+#Matthew McKinley Rock Paper Scissors
+
+import random
+score = 0
+games = 0
+
+def main():
+    
+
+
+
+
+
+
+
+playagain = input("Do you want to play again (Y/N)?")
+if playagain == "Y"
+main()
+el
+
+
+
+
+
+
+
+#choice = int(input("What weapon would you like to use? (1 for Rock, 2 for Paper, 3 for Scissors)"))
+#if choice == 1:
+    #choice = 1
+#elif choice == 2:
+    #choice = 2
+#elif choice == 3:
+    #choice = 3
+#else:
+    #print("You did not write 1, 2, or 3")
+
+
+#computer = random.randint(1,3)
