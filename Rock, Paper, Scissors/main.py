12a34084d5691c9306c9c094754e5f04c9cff2b1
matthew-mckinley1 <matthew.mckinley@ucas-edu.net>
2024-11-05 10:38:46 -0700

    I locked in ahahahahah im back finally that was fun

--- Rock, Paper, Scissors/main.py
+++ Rock, Paper, Scissors/main.py
@@ -1,43 +1,94 @@
 #Matthew McKinley Rock Paper Scissors
 
 import random
-score = 0
-games = 0
+
+wins = int(0)
+games = int(0)
+
 
 def main():
-    
-
-
-
-
-
-
-
-    playagain = input("Do you want to play again (Y/N)?")
-    if playagain == "Y":
-        main()
+    choice = int(input("What weapon would you like to use? (1 for Rock, 2 for Paper, 3 for Scissors)"))
+    if choice == 1:
+        choice = "Rock"
+        print(choice)
+    elif choice == 2:
+        choice = "Paper"
+        print(choice)
+    elif choice == 3:
+        choice = "Scissors"
+        print(choice)
     else:
-        print("Goodbye!")
-        break
+        print("You did not write 1, 2, or 3")
+    comchoice = random.randint(1,3)
+    if comchoice == 1:
+        comchoice = "Rock"
+        print(comchoice)
+    if comchoice == 2:
+        comchoice = "Paper"
+        print(comchoice)
+    if comchoice == 3:
+        comchoice = "Scissors"
+        print(comchoice)
     
 
 
+    def playagain():
+        global games
+        global wins
+        print("Your score is ", wins, "out of ", games)
+        playagain = input("Do you want to play again (Y/N)?")
+        if playagain == "Y":
+            main()
+        elif playagain == "N":
+            print("Goodbye!")
+            exit()
+        else:
+            print("You typed something wrong! Try again :)")
+            playagain()
 
+    def playerwin():
+        global games
+        global wins
+        print("You won!")
+        wins = wins + 1
+        games = games + 1
+        playagain()
 
+    def draw():
+        global games
+        print("You tied!")
+        games = games + 1
+        playagain()
 
+    def playerlose():
+        global games
+        print("You lost!")
+        games = games + 1
+        playagain()
 
+#tie conditions
+    if choice == "Rock" and comchoice == "Rock":
+        draw()
+    elif choice == "Paper" and comchoice == "Paper":
+        draw()
+    elif choice == "Scissors" and comchoice == "Scissors":
+        draw()
+#win conditions
+    elif choice == "Rock" and comchoice == "Scissors":
+        playerwin()
+    elif choice == "Paper" and comchoice == "Rock":
+        playerwin()
+    elif choice == "Scissors" and comchoice == "Paper":
+        playerwin()
+#lose conditions
+    elif choice == "Scissors" and comchoice == "Rock":
+        playerlose()
+    elif choice == "Rock" and comchoice == "Paper":
+        playerlose()
+    elif choice == "Paper" and comchoice == "Scissors":
+        playerlose()
+    else:
+        print("You typed something wrong!")
+        playagain()
 
-
-
-#choice = int(input("What weapon would you like to use? (1 for Rock, 2 for Paper, 3 for Scissors)"))
-#if choice == 1:
-    #choice = 1
-#elif choice == 2:
-    #choice = 2
-#elif choice == 3:
-    #choice = 3
-#else:
-    #print("You did not write 1, 2, or 3")
-
-
-#computer = random.randint(1,3)
+main()
