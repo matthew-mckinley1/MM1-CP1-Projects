10d62aec2c9418d7fbc611cc2a8a92739eeb7ae4
matthew-mckinley1 <matthew.mckinley@ucas-edu.net>
2024-11-14 10:51:14 -0700

    why did you do that

--- Tic Tac TOES/main.py
+++ Tic Tac TOES/main.py
@@ -2,4 +2,21 @@
 
 import random
 
-computer = random.randint(1,11)
+#board
+computer = random.randint(1,10)
+row1 = [" 1 | 2 | 3 "]
+row2 = [" 4 | 5 | 6 "]
+row3 = [" 7 | 8 | 9 "]
+board = [row1, row2,row3]
+for row in board:
+    print(row)
+
+playerspot = int(input("What place would you like to go? (1-9)"))
+
+for row in board:
+    for spot in row:
+        if spot == playerspot:
+            row = row.replace(spot, "X")
+        
+for row in board:
+    print(row)
