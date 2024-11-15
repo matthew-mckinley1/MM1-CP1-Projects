97f9a9050c944c36dd8524b2511e9b3ad810a356
matthew-mckinley1 <matthew.mckinley@ucas-edu.net>
2024-11-15 10:51:03 -0700

    TERIBLE IM SO BAD ATO CONDING

--- Tic Tac TOES/main.py
+++ Tic Tac TOES/main.py
@@ -3,20 +3,44 @@
 import random
 
 #board
-computer = random.randint(1,10)
-row1 = [" 1 | 2 | 3 "]
-row2 = [" 4 | 5 | 6 "]
-row3 = [" 7 | 8 | 9 "]
+wrongspot = True
+row1 = ["1", "2", "3"]
+row2 = ["4", "5", "6"]
+row3 = ["7", "8", "9"]
 board = [row1, row2,row3]
+
 for row in board:
     print(row)
 
-playerspot = int(input("What place would you like to go? (1-9)"))
+def check():
+    #tie
 
-for row in board:
-    for spot in row:
-        if spot == playerspot:
-            row = row.replace(spot, "X")
-        
-for row in board:
-    print(row)
+    #xrow
+    #xcolumn
+    #xdiag
+    #orow
+    #ocolumn
+    #odiag
+
+while True:
+    wrongspot = True
+    playerspot = input("What place would you like to go? (1-9)")
+    for rowindex, row in enumerate(board):
+        for spotindex, spot in enumerate(row):
+            if spot == playerspot:
+                board[rowindex][spotindex] = "X"
+    check()
+    
+    for row in board:
+        print(row)
+    print("")
+    while wrongspot == True:
+        computer = random.randint(1,10)
+        for rowindex, row in enumerate(board):
+            for spotindex, spot in enumerate(row):
+                if spot == str(computer):
+                    board[rowindex][spotindex] = "O"
+                    wrongspot = False
+    check()
+    for row in board:
+        print(row)
