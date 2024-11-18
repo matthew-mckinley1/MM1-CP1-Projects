2abbcf23946c63fa06c5738d6ef7cadc8ae8a12c
matthew-mckinley1 <matthew.mckinley@ucas-edu.net>
2024-11-18 10:51:17 -0700

    FIX THAT NEXT TIME AJIOP JPWI JIW OP

--- Tic Tac TOES/main.py
+++ Tic Tac TOES/main.py
@@ -2,6 +2,9 @@
 
 import random
 
+end = False
+userWin = False
+comWin = False
 #board
 wrongspot = True
 row1 = ["1", "2", "3"]
@@ -13,14 +16,70 @@ for row in board:
     print(row)
 
 def check():
+    global end
+    global userWin
+    global comWin
     #tie
-
+    tie = 0
+    for row in board:
+        for spot in row:
+            if spot == "X" or spot == "O":
+                tie += 1
+    if tie == 9:
+        end = True
     #xrow
+    for row in board:
+        if row == ["X", "X", "X"]:
+            end = True
     #xcolumn
+    columnX = 0
+    for row in board:
+        if row[0] == "X":
+            columnX += 1
+    for row in board:
+        if row[1] == "X":
+            columnX += 1
+    for row in board:
+        if row [2] == "X":
+            columnX += 1
+    if columnX == 3:
+        end = True
+        userWin = True
     #xdiag
+    if board[0][0] == "X" and board[1][1] == "X" and board[2][2] == "X":
+        userWin = True
+        end = True
+    if board[0][2] == "X" and board[1][1] == "X" and board [2][0] == "X":
+        userWin = True
+        end = True
     #orow
+        for row in board:
+            if row == ["O", "O", "O"]:
+                end = True
+                comWin = True
     #ocolumn
+    columnO = 0
+    for row in board:
+        if row[0] == "O":
+            columnO += 1
+    columnO = 0 #FIX THIS NEXT TIME
+    for row in board:
+        if row[1] == "O":
+            columnO += 1
+    for row in board:
+        if row [2] == "O":
+            columnO += 1
+    if columnO == 3:
+        end = True
+        comWin = True
     #odiag
+    if board[0][0] == "O" and board[1][1] == "O" and board[2][2] == "O":
+        comWin = True
+        end = True
+    if board[0][2] == "O" and board[1][1] == "O" and board [2][0] == "O":
+        comWin = True
+        end = True
+    
 
 while True:
     wrongspot = True
@@ -30,7 +89,8 @@ while True:
             if spot == playerspot:
                 board[rowindex][spotindex] = "X"
     check()
-    
+    if end == True:
+        break
     for row in board:
         print(row)
     print("")
@@ -42,5 +102,14 @@ while True:
                     board[rowindex][spotindex] = "O"
                     wrongspot = False
     check()
+    if end == True:
+        break
     for row in board:
         print(row)
+
+if userWin == True:
+    print("YOU WON!!!!")
+elif comWin == True:
+    print("THE COMPUTER WON AND YOU LOST TO A RANDOM COMPUTER OH NO SKILL ISSUE")
+else:
+    print("This is an error code i did something wrong")
